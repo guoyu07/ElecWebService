cf997bc9b4ab5635ab66459cd3cf0d17d47a5c54
gsh199449 <gsh199449@hotmail.com>
2013-12-12 17:20:32 +0800

    Initial Commit

---
+++ src/com/gs/service/CommitService.java
@@ -0,0 +1,18 @@
+package com.gs.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CommitService extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+
+}
